3f83ed98ba80cce6f6ef00ad769989ffe1d09d4b
Duncan <loxa10077@gmail.com>
2019-01-30 00:17:08 +0800

    typo

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,13 +1,13 @@
 class CartsController < ApplicationController
   def add
     current_cart.add_item(params[:id])
-    session[:cart9487] = current_cart.serialize
+    session[:cart9488] = current_cart.serialize
 
     redirect_to products_path, notice: "已加入購物車"
   end
 
   def destroy
-    session[:cart9487] = nil
+    session[:cart9488] = nil
     redirect_to products_path, notice: "購物車已清空"
   end
 end
