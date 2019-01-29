2d2f560661ca54fffc73df541833e459dec1403e
Duncan <loxa10077@gmail.com>
2019-01-30 00:20:12 +0800

    useing Cart::SessionKey

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,13 +1,13 @@
 class CartsController < ApplicationController
   def add
     current_cart.add_item(params[:id])
-    session[:cart9488] = current_cart.serialize
+    session[Cart::SessionKey] = current_cart.serialize
 
     redirect_to products_path, notice: "已加入購物車"
   end
 
   def destroy
-    session[:cart9488] = nil
+    session[Cart::SessionKey] = nil
     redirect_to products_path, notice: "購物車已清空"
   end
 end
