d885a46d42a2dbc482343948e7e0f27ef653fae8
Duncan <loxa10077@gmail.com>
2019-01-29 23:17:56 +0800

    add product and cart route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  'signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :products
+  resource :cart, only:[:show, :destroy] do
+    collection do
+      post :add, path:'add/:id'
+    end
+  end
+
 end
