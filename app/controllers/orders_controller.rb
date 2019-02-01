a57f21d588fcb2a8847a08fdc47407fd43790af1
Duncan <loxa10077@gmail.com>
2019-02-01 13:58:34 +0800

    user can create order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,14 +3,15 @@ class OrdersController < ApplicationController
   def index
     # @orders = current_user.orders.order(created_at: :desc)
     @orders = Order.all
+    # @orders = current_user.orders
   end
 
   def create
     # @order = current_user.orders.new(order_params)
-    @order = Order.new(sn: Time.new.to_i, amount: current_cart.total_price, user_id: current_user.id)
     # @order.sn = Time.now.to_i
-    # @order.add_order_items(current_cart)
     # @order.amount = current_cart.total_price
+    @order = Order.new(sn: Time.new.to_i, amount: current_cart.total_price, user_id: current_user.id)
+    @order.add_order_items(current_cart)
     if @order.save
       # current_cart.destroy
       # session.delete(:new_order_data)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_01_010501) do
+ActiveRecord::Schema.define(version: 2019_02_01_053829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "product_id"
+    t.integer "quantity"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "sn"
@@ -56,5 +67,7 @@ ActiveRecord::Schema.define(version: 2019_02_01_010501) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
   add_foreign_key "orders", "users"
 end
