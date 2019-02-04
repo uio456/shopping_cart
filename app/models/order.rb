5dbc7ca18a514d74eb4ead5d8a7d8ecd85248621
Duncan <loxa10077@gmail.com>
2019-02-04 16:54:04 +0800

    order item page can display item_price and cart_item_price

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,15 +3,14 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :products, through: :order_items
 
-
   def add_order_items(current_cart)
     current_cart.items.each do |cart_item|
       self.order_items.build(
         product_id: cart_item.product.id,
         quantity: cart_item.quantity, 
-        price: cart_item.product.price
-        # price: cart_item.product.price
-        # 可在新增每個 product 的價錢，讓 order_item 顯示單個 product 價錢
+        item_price: cart_item.product.price,
+        cart_item_price: cart_item.price,
+        shipping_fee: current_cart.shipping_fee
       )
     end
   end
