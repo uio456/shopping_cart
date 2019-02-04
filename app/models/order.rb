7bfdc8562aaa1997fca3888eba52cdfd9592617b
Duncan <loxa10077@gmail.com>
2019-02-04 15:09:48 +0800

    improve admin create or edit product

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,7 +9,9 @@ class Order < ApplicationRecord
       self.order_items.build(
         product_id: cart_item.product.id,
         quantity: cart_item.quantity, 
-        price: cart_item.price
+        price: cart_item.product.price
+        # price: cart_item.product.price
+        # 可在新增每個 product 的價錢，讓 order_item 顯示單個 product 價錢
       )
     end
   end
