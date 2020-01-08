13659878f65a436511634e076c7edc2446d7b00d
Duncan <loxa10077@gmail.com>
2020-01-09 00:37:49 +0800

    delete unnecessary migration

--- db/migrate/20200108161404_remove_title_from_promotional.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTitleFromPromotional < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :promotionals, :title
-  end
-end
--- db/migrate/20200108162416_add_column_to_promotional.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToPromotional < ActiveRecord::Migration[5.2]
-  def change
-    add_column :promotionals, :title, :string
-    add_column :promotionals, :order_p, :integer
-  end
-end
