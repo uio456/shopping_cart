5803f2aea7b01ff3e2d1b21ac3b53971e33ab441
Duncan <loxa10077@gmail.com>
2020-01-14 21:39:15 +0800

    add cart_item_discount_price helper method

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,12 @@
 module OrdersHelper
+
+  def cart_item_discount_price
+    price = current_cart.cart_item_discount_price
+    if price == 0
+      content = "你還沒買！趕快買"
+    else
+      content = price
+    end
+     "顯示目前享有折扣金額：#{content}"
+  end
 end
