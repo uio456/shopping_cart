986282feca7bae9846af72cc9765811071dcef5b
Duncan <loxa10077@gmail.com>
2020-01-16 00:14:54 +0800

    add item_discount_price helper method

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -4,4 +4,22 @@ module CartsHelper
     @cart ||= Cart.from_hash(session[Cart::SessionKey])
   end
 
+  def item_discount_price(item)
+    promotion = Promotional.first
+    item_quantity = item.quantity
+
+    case item.product.state
+    when "item_p"
+      if item_quantity >= promotion.item_p
+        return Discount.cart_item_discount_price(item.product, item_quantity)
+      end
+    when "vendor_p"
+      if item_quantity >= promotion.vendor_p
+        return Discount.cart_item_discount_price(item.product, item_quantity)
+      end
+    end
+
+    return  "-"
+  end
+
 end
