743ff71b153f7ae940df7b54932da2f804f1978d
Duncan <loxa10077@gmail.com>
2020-01-15 23:53:52 +0800

    optimize cart show page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,12 +2,11 @@ class CartsController < ApplicationController
   def add
     current_cart.add_item(params[:id])
     # current_cart 來源自，CartsHelper
-    # @cart ||= Cart.from_hash(session[Cart::SessionKey])
-    if current_cart.total_price >= 800
-      # 800 的金額應該寫進 Promotional 裡
-      # 後台沒做滿額送產品的介面
-      Discount.free_product(current_cart)
-    end
+    # if current_cart.total_price >= 800
+    #   # 800 的金額應該寫進 Promotional 裡
+    #   # 後台沒做滿額送產品的介面
+    #   Discount.free_product(current_cart)
+    # end
     session[Cart::SessionKey] = current_cart.serialize
     # 最後存進 session 裡
 
@@ -16,8 +15,6 @@ class CartsController < ApplicationController
 
   def show
     @order = Order.new
-    # @cart = current_cart.check_discount(current_cart)
-    # @filter_cart = DiscountCart.new(current_cart).perform
   end
 
   def destroy
