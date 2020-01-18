3bd969dd38fd8efa69f76e83d020a5f049568aee
Duncan <loxa10077@gmail.com>
2020-01-18 11:43:33 +0800

    add send_file method to Mailer Class

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,6 +1,16 @@
 class Mailer < ApplicationMailer
+  #除非對方授權，不然免錢的 mailgun 不能隨便寄到別人的電子信箱
   def say_hi(user)
     @user = user
-    mail to:"loxa10077@gmail.com", subject:"你好!!"
+    mail to: @user.email, subject:"你好!!"
   end
+
+  def send_file(to, subject, body, files:{})
+    files.each do |name, file|
+      attachments[name] = file.read
+    end
+
+    mail to: to, subject: subject, body: body
+  end
+
 end
