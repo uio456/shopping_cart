9f5c2fe2b44a78ac7bfb131be021b2d365f3628d
Duncan <loxa10077@gmail.com>
2020-01-29 19:51:38 +0800

    update admin page

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
   has_many :taggings, dependent: :destroy
   has_many :products, through: :taggings
-  belongs_to :weather_collect
+  belongs_to :weather_collect, optional: true
 end
