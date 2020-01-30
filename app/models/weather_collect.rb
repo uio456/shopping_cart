66969774c449ec4f0d539ed426d55b12c398acf5
Duncan <loxa10077@gmail.com>
2020-01-30 11:26:07 +0800

    refactor get_weather_products service

--- app/models/weather_collect.rb
+++ app/models/weather_collect.rb
@@ -1,3 +1,5 @@
 class WeatherCollect < ApplicationRecord
   has_many :tags, dependent: :destroy
+
+  WEATHER_GROUP = ['夏天群組', '冷天群組', '涼爽群組', '雨天群組' ]
 end
--- app/services/get_weather_products.rb
+++ app/services/get_weather_products.rb
@@ -9,34 +9,25 @@ class GetWeatherProducts
     end
 
     def find_fitting_products
-
-      # 這邊應該要用 join 來拉資料
-      # @weather_info 拿資料
-
       case @weather_info[:weather_info][:desc]
-      when '稍有寒意至舒適', '寒冷至舒適'
-        tag_names =  ["保暖", "輕便"]
+      when '稍有寒意至舒適', '寒冷至舒適', '稍有寒意', '寒冷', '寒冷至稍有寒意', '非常寒冷至寒冷', '非常寒冷至稍有寒意'
+        tag_names = WeatherCollect.find_by(name: WeatherCollect::WEATHER_GROUP[1]).tags.pluck(:name) # 冷天群組
       when '舒適'
-        tag_names =  ["輕便"]
-      when '稍有寒意', '寒冷', '寒冷至稍有寒意', '非常寒冷至寒冷', '非常寒冷至稍有寒意'
-        tag_names =  ["保暖", "防寒"]
+        tag_names = WeatherCollect.find_by(name: WeatherCollect::WEATHER_GROUP[2]).tags.pluck(:name) # 涼爽群組
+      when '防曬'
+        tag_names = WeatherCollect.find_by(name: WeatherCollect::WEATHER_GROUP[0]).tags.pluck(:name) # 夏天群組
       else
         tag_names = []
       end
 
-      if @weather_info[:weather_info][:probability_of_recipitation].to_i > 40 # 大於 40 下雨機率
-        tag_names += ["下雨"]
+      # 雨天標籤，下雨機率大於 40
+      if @weather_info[:weather_info][:probability_of_recipitation].to_i > 40
+        tag_names += WeatherCollect.find_by(name: WeatherCollect::WEATHER_GROUP[3]).tags.pluck(:name) # 雨天群組
       end
-
-      # 在這裡決定拿哪一個 product_collect 出來撈資料
-      # product_collect 暫定四種 1.熱天 2.涼天 3.冷天 4.下雨天
-      # 裡面有一個 array 存各個天氣的關鍵字
-      # 只有 superman 可以更改
 
       product_ids = Tagging.where(tag_id: Tag.where(name: tag_names).select(:id)).select(:product_id)
 
       @products.where(id: product_ids)
-
     end
 
   end
