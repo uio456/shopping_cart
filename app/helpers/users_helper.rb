cba23248e68da71138fcb2f6322bdf91c8a761fd
Duncan <loxa10077@gmail.com>
2020-02-01 00:04:14 +0800

    fix admin page Bug

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,7 @@
 module UsersHelper
 
-  def which_vendor
-    if current_user.vendor_admin?
-      Vendor.where(id: current_user.vendor_id)
-    else
-      Vendor.all
-    end
+  def which_vendor(id)
+    id.present? ? Vendor.find(id).name : "自家管理員"
   end
 
   def superman_or_vendor_admin?
@@ -17,7 +13,7 @@ module UsersHelper
       if user.vendor_id.present?
         User::ROLE[1..2]
       else
-        User::ROLE[0..1]
+        User::ROLE[0..2]
       end
     else
       User::ROLE[0..1]
