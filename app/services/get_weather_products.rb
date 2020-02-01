b6cc00035b198f50fe2bf312260b7e9f4499252c
Duncan <loxa10077@gmail.com>
2020-02-01 09:31:24 +0800

    update weather crawler

--- app/services/get_weather_products.rb
+++ app/services/get_weather_products.rb
@@ -10,7 +10,7 @@ class GetWeatherProducts
 
     def find_fitting_products
       case @weather_info[:weather_info][:desc]
-      when '稍有寒意至舒適', '寒冷至舒適', '稍有寒意', '寒冷', '寒冷至稍有寒意', '非常寒冷至寒冷', '非常寒冷至稍有寒意'
+      when '稍有寒意至舒適', '寒冷至舒適', '稍有寒意', '寒冷', '寒冷至稍有寒意', '非常寒冷至寒冷', '非常寒冷至舒適', '非常寒冷至稍有寒意'
         tag_names = get_weather_group(1) # 1 是冷天群組
       when '舒適'
         tag_names = get_weather_group(2) # 2 是涼爽群組
