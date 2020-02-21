daab9640895666269ad9c0166d91a6bb4cacdfe6
Duncan <loxa10077@gmail.com>
2020-02-21 14:27:19 +0800

    add authentication_token cloumn for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
+  before_create :generate_authentication_token
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   # email 驗證格式
   # validates :email, presence: true, format: { with: /\A[\w+\-.\[\]]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}, uniqueness: true
 
@@ -34,6 +34,10 @@ class User < ApplicationRecord
 
   def vendor_admin?
     vendor_id.present? && admin?
+  end
+
+  def generate_authentication_token
+    self.authentication_token = Devise.friendly_token
   end
 
 end
